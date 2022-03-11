adb7e8849051f8b8a8c745b3d3fa2704e92b9b36
ajtock <andytock@gmail.com>
2022-03-11 17:20:05 +0000

    second commit

---
+++ BSseq_leaf_Hazarika_Johannes_2022_NatPlants/snakemake_BSseq_t2t-col.20210610/data/MA1_2_wget.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342347/SRR342347.fastq.gz
+mv SRR342347.fastq.gz MA1_2_G3_L1_BSseq_Rep1_SRR342347.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342382/SRR342382.fastq.gz
+mv SRR342382.fastq.gz MA1_2_G3_L1_BSseq_Rep2_SRR342382.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342353/SRR342353.fastq.gz
+mv SRR342353.fastq.gz MA1_2_G3_L12_BSseq_Rep1_SRR342353.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342354/SRR342354.fastq.gz
+mv SRR342354.fastq.gz MA1_2_G3_L12_BSseq_Rep2_SRR342354.fastq.gz 
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342348/SRR342348.fastq.gz
+mv SRR342348.fastq.gz MA1_2_G3_L19_BSseq_Rep1_SRR342348.fastq.gz 
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342383/SRR342383.fastq.gz
+mv SRR342383.fastq.gz MA1_2_G3_L19_BSseq_Rep2_SRR342383.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342349/SRR342349.fastq.gz
+mv SRR342349.fastq.gz MA1_2_G31_L29_BSseq_Rep1_SRR342349.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342384/SRR342384.fastq.gz
+mv SRR342384.fastq.gz MA1_2_G31_L29_BSseq_Rep2_SRR342384.fastq.gz  
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342378/SRR342378.fastq.gz 
+mv SRR342378.fastq.gz MA1_2_G31_L49_BSseq_Rep1_SRR342378.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342385/SRR342385.fastq.gz 
+mv SRR342385.fastq.gz MA1_2_G31_L49_BSseq_Rep2_SRR342385.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342379/SRR342379.fastq.gz 
+mv SRR342379.fastq.gz MA1_2_G31_L59_BSseq_Rep1_SRR342379.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342389/SRR342389.fastq.gz
+mv SRR342389.fastq.gz MA1_2_G31_L59_BSseq_Rep2_SRR342389.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342381/SRR342381.fastq.gz
+mv SRR342381.fastq.gz MA1_2_G31_L119_BSseq_Rep1_SRR342381.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR342/SRR342391/SRR342391.fastq.gz
+mv SRR342391.fastq.gz MA1_2_G31_L119_BSseq_Rep2_SRR342391.fastq.gz
+
