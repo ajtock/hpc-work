5fec579e41c054253fec4cd5710b074e013b1055
ajtock <andytock@gmail.com>
2022-03-21 19:23:54 +0000

    alphabeta

--- BSseq_leaf_Hazarika_Johannes_2022_NatPlants/snakemake_BSseq_t2t-col.20210610/scripts/alphabeta_per_cytosine_MA1_2.R
+++ BSseq_leaf_Hazarika_Johannes_2022_NatPlants/snakemake_BSseq_t2t-col.20210610/scripts/alphabeta_per_cytosine_MA1_2.R
@@ -374,7 +374,7 @@ targetDF_list <- mclapply(1:nrow(binDF), function(i) {
   ##stopifnot(all.equal(ABneutral_BOOTout, ABneutral_BOOToutTest))
   ##rm(ABneutral_BOOToutTest); gc()
 
-}, mc.cores = 70, mc.preschedule = F)
+}, mc.cores = 24, mc.preschedule = T)
 
 
 #targetDF <- dplyr::bind_rows(targetDF_list)
