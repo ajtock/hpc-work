a0c1f0d1875c01edb3b1c644652e68b6ba90a535
ajtock <andytock@gmail.com>
2022-03-25 20:02:14 +0000

    alphabeta

--- BSseq_leaf_Hazarika_Johannes_2022_NatPlants/snakemake_BSseq_t2t-col.20210610/scripts/alphabeta_per_cytosine_MA1_2_dopar_OLD.R
+++ BSseq_leaf_Hazarika_Johannes_2022_NatPlants/snakemake_BSseq_t2t-col.20210610/scripts/alphabeta_per_cytosine_MA1_2_dopar_OLD.R
@@ -22,8 +22,8 @@ chrName <- args[5]
 
 #refbase <- "t2t-col.20210610"
 #context <- "CpG"
-#genomeBinSize <- 10000
-#genomeStepSize <- 10000
+#genomeBinSize <- 1000000
+#genomeStepSize <- 100000
 #chrName <- "Chr1"
 
 options(stringsAsFactors = F)
@@ -32,8 +32,6 @@ library(AlphaBeta)
 library(dplyr)
 library(data.table)
 library(parallel)
-library(doParallel)
-library(iterators)
 library(yaml)
 config <- read_yaml("config.yaml")
 
@@ -86,16 +84,16 @@ chrLens <- fai[,2][which(fai[,1] %in% chrName)]
 # Define paths to methylome TXT files generated with methimpute
 filePathsGlobal <- paste0(inDir, config$SAMPLES, "_MappedOn_", refbase, "_dedup_", context, "_methylome.txt")
 
-## Load methylome TXT files for subsetting cytosines (rows) by genomic bin
-## NOTE: might be worth changing methimpute script to filter out cytosines
-## that are below a given coverage threshold;
-## e.g., >= 4; see https://www.pnas.org/doi/10.1073/pnas.1424254112
-## However, coverage threshold in https://www.nature.com/articles/s41477-021-01086-7
-## seems to be >= 1 (as implemented by default in MethylStar) and with a maximum posterior probability >= 0.99 
-#methylomesGlobalList <- lapply(1:length(filePathsGlobal), function(x) {
-#  fread(filePathsGlobal[x])
-#})
-##}, mc.cores = length(filePathsGlobal), mc.preschedule = F)
+# Load methylome TXT files for subsetting cytosines (rows) by genomic bin
+# NOTE: might be worth changing methimpute script to filter out cytosines
+# that are below a given coverage threshold;
+# e.g., >= 4; see https://www.pnas.org/doi/10.1073/pnas.1424254112
+# However, coverage threshold in https://www.nature.com/articles/s41477-021-01086-7
+# seems to be >= 1 (as implemented by default in MethylStar) and with a maximum posterior probability >= 0.99 
+methylomesGlobalList <- lapply(1:length(filePathsGlobal), function(x) {
+  fread(filePathsGlobal[x])
+})
+#}, mc.cores = length(filePathsGlobal), mc.preschedule = F)
 
 # Define genomic windows
 binDF <- data.frame()
@@ -125,52 +123,48 @@ for(i in 1:length(chrs)) {
   binDF <- rbind(binDF, chr_binDF)
 }
 
-nrow_binDF <- nrow(binDF)
 
-num_cores <- 48 - 2
-
-#binDF <- binDF[1:(num_cores*2),]
-#nrow_binDF <- nrow(binDF)
-
-cl <- makeCluster(num_cores, type = "FORK")
-registerDoParallel(cl)
+library(doParallel)
+num_cores <- detectCores() - 2
+cl <- makeForkCluster(num_cores)
+registerDoParallel(cl)  
 print("Currently registered parallel backend name, version and cores")
 print(getDoParName())
 print(getDoParVersion())
 print(getDoParWorkers())
 
+targetDF <- foreach(i = 1:nrow(binDF), .combine = rbind, .maxcombine = 2e5, .inorder = F) %dopar% {
 
-start <- proc.time()
-targetDF <- foreach(i = icount(nrow_binDF), .combine = 'rbind', .maxcombine = nrow_binDF+1e1, .inorder = F) %dopar% {
+#  options(stringsAsFactors = F)
+#  options(scipen=999)
+#  library(AlphaBeta)
+#  library(dplyr)
+#  library(data.table)
+#  library(parallel)
 
+#  print(i)
   bin_i <- binDF[i,]
   filePaths_bin_i_trunc <- gsub(pattern = "methylome.txt", replacement = paste0("methylome_", paste0(bin_i, collapse = "_"), ".txt"),
                                 x = gsub(pattern = "coverage/report/methimpute/", replacement = "",
                                          x = filePathsGlobal))
   filePaths_bin_i <- paste0(inDirBin, filePaths_bin_i_trunc)
 
-  rm(filePaths_bin_i_trunc); gc()
-
   # Write filePaths_bin_i
   # NOTE: remove these files after use by buildPedigree() due to large file numbers (> 1M)
-  for(x in 1:length(filePathsGlobal)) {
+#  mclapply(1:length(methylomesGlobalList), function(x) {
+  for(x in 1:length(methylomesGlobalList)) {
 
-    methylomesGlobal_x <- fread(filePathsGlobal[x])
-
-    methylome_bin_i <- methylomesGlobal_x %>%
+    methylome_bin_i <- methylomesGlobalList[[x]] %>%
       dplyr::filter(seqnames == bin_i$chr) %>%
       dplyr::filter(start >= bin_i$start & start <= bin_i$end)
-
-    rm(methylomesGlobal_x); gc()
 
     fwrite(methylome_bin_i,
            file = filePaths_bin_i[x],
            quote = F, sep = "\t", row.names = F, col.names = T)
-
-    rm(methylome_bin_i); gc()
   }
+#  }, mc.cores = length(methylomesGlobalList), mc.preschedule = F)
 
-  
+
   # Extract node, generation and methylome info from filePaths_bin_i
   node <- gsub(paste0(inDirBin, "MA\\d+_\\d+_G"), "", filePaths_bin_i)
   node <- gsub("_SRR.+", "", node)
@@ -197,9 +191,6 @@ targetDF <- foreach(i = icount(nrow_binDF), .combine = 'rbind', .maxcombine = nr
                            node = node,
                            gen = generation,
                            meth = methylome)
-
-  rm(filePaths_bin_i, generation, methylome); gc()
-
   node_df_G0 <- data.frame(filename = "-",
                            node = "0_0",
                            gen  = as.integer(0),
@@ -215,18 +206,12 @@ targetDF <- foreach(i = icount(nrow_binDF), .combine = 'rbind', .maxcombine = nr
                                         node[ grep( "Rep1", node ) ] [ grep( "^31_", node[ grep( "Rep1", node ) ] ) ] ),
                             gen = as.integer(30),
                             meth = "N") 
-
-  rm(node); gc()
-
   node_df_G31 <- node_df_GM[node_df_GM$gen == 31,]
-  ##
-  rm(node_df_GM); gc()
-  ##
+  
+  rm(node_df_GM)
+  
   # Combine node generations into one data.frame
   node_df <- dplyr::bind_rows(mget(sort(ls(pattern = "node_df"))))
-
-  rm(node_df_G0, node_df_G2, node_df_G3, node_df_G30, node_df_G31); gc()
-
   node_file <- paste0(outDir, "nodelist_MA1_2_MappedOn_", refbase, "_", context, "_",
                       paste0(bin_i, collapse = "_"), ".fn")
   # NOTE: remove this file after use by buildPedigree() due to large file numbers (> 100k)
@@ -267,13 +252,8 @@ targetDF <- foreach(i = icount(nrow_binDF), .combine = 'rbind', .maxcombine = nr
                            )
     edge_df_GN <- rbind(edge_df_GN, edge_df_x)
   }
-
-  rm(node_df); gc()
- 
+  
   edge_df <- rbind(edge_df_G0, edge_df_GN)
-
-  rm(edge_df_G0, edge_df_GN); gc()
-
   #edge_df$line <- as.numeric( paste0(
   ##                                   gsub("_", "",
   ##                                        gsub("_Rep.+", "",
@@ -293,8 +273,6 @@ targetDF <- foreach(i = icount(nrow_binDF), .combine = 'rbind', .maxcombine = nr
   dropCols <- c("gento")
   edge_df <- edge_df[ , !(names(edge_df) %in% dropCols)]
   
-  rm(dropCols); gc()
-
   edge_file <- paste0(outDir, "edgelist_MA1_2_MappedOn_", refbase, "_", context, "_",
                       paste0(bin_i, collapse = "_"), ".fn")
   # NOTE: remove this file after use by buildPedigree() due to large file numbers (> 100k)
@@ -302,53 +280,40 @@ targetDF <- foreach(i = icount(nrow_binDF), .combine = 'rbind', .maxcombine = nr
          file = edge_file,
          quote = F, sep = "\t", row.names = F, col.names = T)
 
-  rm(edge_df); gc()
 
-
-  ## Build the pedigree of the MA lines in the given genomic bin
-  ##invisible(capture.output(buildPedigree_out <- suppressMessages(buildPedigree(nodelist = node_file,
-  ##                                                                             edgelist = edge_file,
-  ##                                                                             cytosine = sub("p", "", context),
-  ##                                                                             posteriorMaxFilter = 0.99))))
+  # Build the pedigree of the MA lines in the given genomic bin
+  #buildPedigree_file <- paste0(outDir, "buildPedigree_MA1_2_MappedOn_", refbase, "_", context, "_",
+  #                             paste0(bin_i, collapse = "_"), ".RData")
+  #invisible(capture.output(buildPedigree_out <- suppressMessages(buildPedigree(nodelist = node_file,
+  #                                                                             edgelist = edge_file,
+  #                                                                             cytosine = sub("p", "", context),
+  #                                                                             posteriorMaxFilter = 0.99))))
   buildPedigree_out <- buildPedigree(nodelist = node_file,
                                      edgelist = edge_file,
                                      cytosine = sub("p", "", context),
                                      posteriorMaxFilter = 0.99)
   system(paste0("rm ", inDirBin, "*", "_", paste0(bin_i, collapse = "_"), ".txt"))
   system(paste0("rm ", node_file, " ", edge_file))
-
-  rm(node_file, edge_file); gc()
+  ## NOTE: remove this file after use by buildPedigree() due to large file numbers (> 100k)
+  #save(buildPedigree_out,
+  #     file = buildPedigree_file)
 
 
   # Get the mean, minimum and maximum methylation divergence values at delta.t = 62
   # (MA1_2_mean.D, MA1_2_min.D, MA1_2_max.D)
   pedigree <- buildPedigree_out$Pdata
   delta.t <- pedigree[, 2] + pedigree[, 3] - 2 * pedigree[, 1]
-  pedigree <- data.frame(pedigree, delta.t)
-
-  rm(delta.t); gc()
-
+  pedigree <- data.frame(pedigree, delta.t) 
   D_at_dt62 <- pedigree[pedigree$delta.t == 62,]$D.value
-
-  rm(pedigree); gc()
-
   MA1_2_mean.D <- mean(D_at_dt62, na.rm = T)
   MA1_2_min.D <- min(D_at_dt62, na.rm = T)
   MA1_2_max.D <- max(D_at_dt62, na.rm = T)
-
-  rm(D_at_dt62); gc()
 
   data.frame(bin_i,
              MA1_2_mean.D,
              MA1_2_min.D,
              MA1_2_max.D)
-
-  rm(bin_i, MA1_2_mean.D, MA1_2_min.D, MA1_2_max.D); gc()
-
 }
-dopar_loop <- proc.time()-start
-stopCluster(cl)
-
 
 #targetDF <- dplyr::bind_rows(targetDF_list)
 #targetDF <- do.call(rbind, targetDF_list)
@@ -357,7 +322,7 @@ fwrite(targetDF,
                      "_MA1_2_MappedOn_", refbase, "_", chrName, "_", context, ".tsv"),
        quote = F, sep = "\t", row.names = F, col.names = T)
 
-
+stopCluster(cl)
 
   # Plot the pedigree of the MA lines
   
