d337b5f3b4abb7708b2620c32cc0e19632a18dd8
ajtock <andytock@gmail.com>
2022-08-16 15:09:00 +0100

    nanopore_recomb

---
+++ pancentromere/centromeric_coordinates/nanopore_recomb/bedtools_getfasta.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Extract nucleotide sequence at given coordinates (specified in a BED file)
+# in the genome in fasta format
+
+# Note that BED files use 0-based half-open coordinates;
+# start coordinates are 0-based and end coordinates are 1-based,
+# such that a feature START coordinate that cooresponds to the first base in
+# a chromosome is numbered 0, while a feature END coordinate that corresponds
+# to the second base in a chromosome is numbered 2 in the BED file:
+# see https://genome.ucsc.edu/FAQ/FAQformat.html#format1
+
+# Usage:
+# ./bedtools_getfasta.sh /home/ajt200/rds/hpc-work/pancentromere/assemblies/Col-0.ragtag_scaffolds.fa Col-0.ragtag_scaffolds_centromeres 
+# ./bedtools_getfasta.sh /home/ajt200/rds/hpc-work/pancentromere/assemblies/Ler-0_110x.ragtag_scaffolds.fa Ler-0_110x.ragtag_scaffolds_centromeres 
+
+genome=$1
+prefix=$2
+
+source ~/.bashrc
+
+conda activate python_3.9.6
+
+bedtools getfasta -fi ${genome} \
+                  -bed ${prefix}.bed \
+                  -fo ${prefix}.fa \
+                  -name
+
+conda deactivate
---
+++ pancentromere/centromeric_coordinates/nanopore_recomb/get_centromere_coordinates.R
@@ -0,0 +1,81 @@
+#!/usr/bin/env Rscript
+
+# Get centromere and non-centromere coordinates and output in BED format
+
+# Usage:
+# conda activate R-4.1.2
+# ./get_centromere_coordinates.R Col-0.ragtag_scaffolds 'Chr1,Chr2,Chr3,Chr4,Chr5'
+# ./get_centromere_coordinates.R Ler-0_110x.ragtag_scaffolds 'Chr1,Chr2,Chr3,Chr4,Chr5'
+# conda deactivate
+
+#accName <- "Col-0.ragtag_scaffolds"
+#accName <- "Ler-0_110x.ragtag_scaffolds"
+#chrName <- unlist(strsplit("Chr1,Chr2,Chr3,Chr4,Chr5",
+#                           split = ","))
+
+args <- commandArgs(trailingOnly = T)
+accName <- args[1]
+chrName <- unlist(strsplit(args[2],
+                           split = ","))
+
+options(stringsAsFactors = F)
+
+# Define chromosomes and chromosome lengths
+acc_chrs <- read.table(paste0("/home/ajt200/rds/hpc-work/pancentromere/assemblies/",
+                              accName, ".fa.fai"),
+                       header = F)[,1]
+acc_chrs <- gsub("_RagTag_RagTag", "", acc_chrs)
+acc_chrs <- gsub("chr", "Chr", acc_chrs)
+acc_chrs <- gsub("SUPER_", "Chr", acc_chrs)
+acc_chrLens <- read.table(paste0("/home/ajt200/rds/hpc-work/pancentromere/assemblies/",
+                                 accName, ".fa.fai"),
+                          header = F)[,2]
+acc_chrLens <- acc_chrLens[which(acc_chrs %in% chrName)]
+acc_chrs <- acc_chrs[which(acc_chrs %in% chrName)]
+print(acc_chrs)
+print(acc_chrLens)
+acc_chrLens <- acc_chrLens[sort.int(acc_chrs, index.return = T)$ix]
+acc_chrs <- acc_chrs[sort.int(acc_chrs, index.return = T)$ix]
+print(acc_chrs)
+print(acc_chrLens)
+
+# Load CEN coordinates
+CEN <- read.csv(paste0("/home/ajt200/rds/hpc-work/pancentromere/centromeric_coordinates/",
+                       "centromere_manual_EDTA4_fa.csv"),
+                header = T)
+CEN$fasta.name <- gsub(".fa", "", CEN$fasta.name)
+acc_CEN <- CEN[grep(accName, CEN$fasta.name),]
+acc_CEN <- acc_CEN[,which(colnames(acc_CEN) %in% c("chr", "start", "end"))]
+acc_CEN_new <- data.frame()
+for(i in 1:length(acc_chrs)) {
+  acc_CEN_chr <- acc_CEN[which(acc_CEN$chr == acc_chrs[i]),]
+  if(nrow(acc_CEN_chr) > 1) {
+    acc_CEN_chr <- data.frame(chr = acc_CEN_chr$chr[1],
+                              start = acc_CEN_chr$start[1],
+                              end = acc_CEN_chr$end[nrow(acc_CEN_chr)])
+  }
+  acc_CEN_new <- rbind(acc_CEN_new, acc_CEN_chr)
+}
+acc_CEN <- acc_CEN_new 
+
+acc_nonCEN <- data.frame(chr = rep(acc_CEN$chr, 2),
+                         start = c(rep(1, 5), acc_CEN$end+1),
+                         end = c(acc_CEN$start-1, acc_chrLens))
+
+# Order first by chromosome, then start, then end
+acc_CEN <- acc_CEN[ with(acc_CEN, order(chr, start, end)), ]
+acc_nonCEN <- acc_nonCEN[ with(acc_nonCEN, order(chr, start, end)), ]
+
+# Substract 1 from start coordinates for output in BED format
+acc_CEN$start <- acc_CEN$start-1
+acc_nonCEN$start <- acc_nonCEN$start-1
+
+# Output in BED format
+write.table(acc_CEN,
+            file = paste0(accName,
+                          "_centromeres.bed"),
+            quote = F, sep = "\t", row.names = F, col.names = F)
+write.table(acc_nonCEN,
+            file = paste0(accName,
+                          "_not_centromeres.bed"),
+            quote = F, sep = "\t", row.names = F, col.names = F)
