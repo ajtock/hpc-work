abb79e8acb2d9a196e290e6ecb33c5f726fb7b72
ajtock <andytock@gmail.com>
2022-10-07 12:50:14 +0100

    not_centromeres

--- pancentromere/centromeric_coordinates/nanopore_recomb/not_centromeres/kmers_in_acc1_not_in_acc2.py
+++ pancentromere/centromeric_coordinates/nanopore_recomb/not_centromeres/kmers_in_acc1_not_in_acc2.py
@@ -185,119 +185,98 @@ venn_fig.savefig(plotDir + "/venn.png")
 #hash_venn_fig.savefig(plotDir + "/venn_hash.png")
 
 
-## acc1in (acc1 centromere-specific k-mers)
+## acc1c_kmers (acc1 centromere-specific k-mers)
 # Define union of acc1notcen, acc2cen, acc2notcen and mitochloro
 acc1notcen_acc2cen_acc2notcen_mitochloro_union = union_lists(acc1notcen, acc2cen, acc2notcen, mitochloro)
 
 # Get k-mers unique to acc1cen by computing difference between
 # acc1cen and the above-defined union
-# (i.e., acc1cen \ acc1notcen_acc2cen_acc2notcen_mitochloro_union): acc1in
-acc1in = list(set(acc1cen).difference(set(acc1notcen_acc2cen_acc2notcen_mitochloro_union)))
-acc1in = sorted(acc1in)
-print(len(acc1in))
+# (i.e., acc1cen \ acc1notcen_acc2cen_acc2notcen_mitochloro_union): acc1c_kmers
+acc1c_kmers = list(set(acc1cen).difference(set(acc1notcen_acc2cen_acc2notcen_mitochloro_union)))
+acc1c_kmers = sorted(acc1c_kmers)
+print(len(acc1c_kmers))
 # 309849
-print(len(acc1cen) - len(acc1in))
+print(len(acc1cen) - len(acc1c_kmers))
 # 350682
 
 # Convert into dictionary
-acc1in_1tolen = list(range(1, len(acc1in)+1)) 
-acc1in_dict = dict(zip(acc1in_1tolen, acc1in))
+acc1c_kmers_1tolen = list(range(1, len(acc1c_kmers)+1)) 
+acc1c_kmers_dict = dict(zip(acc1c_kmers_1tolen, acc1c_kmers))
 
 
-# acc2in (acc2 centromere-specific k-mers)
+# acc2c_kmers (acc2 centromere-specific k-mers)
 # Define union of acc2notcen, acc1cen, acc1notcen and mitochloro
 acc2notcen_acc1cen_acc1notcen_mitochloro_union = union_lists(acc2notcen, acc1cen, acc1notcen, mitochloro)
 
 # Get k-mers unique to acc2cen by computing difference between
 # acc2cen and the second above-defined union
-# (i.e., acc2cen \ acc2notcen_acc1cen_acc1notcen_mitochloro_union): acc2in
-acc2in = list(set(acc2cen).difference(set(acc2notcen_acc1cen_acc1notcen_mitochloro_union)))
-acc2in = sorted(acc2in)
-print(len(acc2in))
+# (i.e., acc2cen \ acc2notcen_acc1cen_acc1notcen_mitochloro_union): acc2c_kmers
+acc2c_kmers = list(set(acc2cen).difference(set(acc2notcen_acc1cen_acc1notcen_mitochloro_union)))
+acc2c_kmers = sorted(acc2c_kmers)
+print(len(acc2c_kmers))
 # 353341 
-print(len(acc2cen) - len(acc2in))
+print(len(acc2cen) - len(acc2c_kmers))
 # 306093
 
 # Convert into dictionary
-acc2in_1tolen = list(range(1, len(acc2in)+1)) 
-acc2in_dict = dict(zip(acc2in_1tolen, acc2in))
+acc2c_kmers_1tolen = list(range(1, len(acc2c_kmers)+1)) 
+acc2c_kmers_dict = dict(zip(acc2c_kmers_1tolen, acc2c_kmers))
 
 
-# acc1out (acc1 non-centromere-specific k-mers)
+# acc1nc_kmers (acc1 non-centromere-specific k-mers)
 # Define union of acc1cen, acc2cen, acc2notcen and mitochloro
 acc1cen_acc2cen_acc2notcen_mitochloro_union = union_lists(acc1cen, acc2cen, acc2notcen, mitochloro)
 
 # Get k-mers unique to acc1notcen by computing difference between
 # acc1notcen and the above-defined union
-# (i.e., acc1notcen \ acc1cen_acc2cen_acc2notcen_mitochloro_union): acc1out
-acc1out = list(set(acc1notcen).difference(set(acc1cen_acc2cen_acc2notcen_mitochloro_union)))
-acc1out = sorted(acc1out)
-print(len(acc1out))
+# (i.e., acc1notcen \ acc1cen_acc2cen_acc2notcen_mitochloro_union): acc1nc_kmers
+acc1nc_kmers = list(set(acc1notcen).difference(set(acc1cen_acc2cen_acc2notcen_mitochloro_union)))
+acc1nc_kmers = sorted(acc1nc_kmers)
+print(len(acc1nc_kmers))
 # 19183610 
-print(len(acc1notcen) - len(acc1out))
+print(len(acc1notcen) - len(acc1nc_kmers))
 # 90804814 
 
 # Convert into dictionary
-acc1out_1tolen = list(range(1, len(acc1out)+1)) 
-acc1out_dict = dict(zip(acc1out_1tolen, acc1out))
+acc1nc_kmers_1tolen = list(range(1, len(acc1nc_kmers)+1)) 
+acc1nc_kmers_dict = dict(zip(acc1nc_kmers_1tolen, acc1nc_kmers))
 
 
-# acc2out (acc2 non-centromere-specific k-mers)
+# acc2nc_kmers (acc2 non-centromere-specific k-mers)
 # Define union of acc1cen, acc2cen, acc1notcen and mitochloro
 acc1cen_acc2cen_acc1notcen_mitochloro_union = union_lists(acc1cen, acc2cen, acc1notcen, mitochloro)
 
 # Get k-mers unique to acc2notcen by computing difference between
 # acc2notcen and the above-defined union
-# (i.e., acc2notcen \ acc1cen_acc2cen_acc1notcen_mitochloro_union): acc2out
-acc2out = list(set(acc2notcen).difference(set(acc1cen_acc2cen_acc1notcen_mitochloro_union)))
-acc2out = sorted(acc2out)
-print(len(acc2out))
+# (i.e., acc2notcen \ acc1cen_acc2cen_acc1notcen_mitochloro_union): acc2nc_kmers
+acc2nc_kmers = list(set(acc2notcen).difference(set(acc1cen_acc2cen_acc1notcen_mitochloro_union)))
+acc2nc_kmers = sorted(acc2nc_kmers)
+print(len(acc2nc_kmers))
 # 19452914 
-print(len(acc2notcen) - len(acc2out))
+print(len(acc2notcen) - len(acc2nc_kmers))
 # 90568109
 
 # Convert into dictionary
-acc2out_1tolen = list(range(1, len(acc2out)+1)) 
-acc2out_dict = dict(zip(acc2out_1tolen, acc2out))
+acc2nc_kmers_1tolen = list(range(1, len(acc2nc_kmers)+1)) 
+acc2nc_kmers_dict = dict(zip(acc2nc_kmers_1tolen, acc2nc_kmers))
 
 
 # Write to FASTA
-write_fasta(kmer_dict=acc1in_dict,
+write_fasta(kmer_dict=acc1c_kmers_dict,
             acc_name=parser.acc1c[0:5],
             outfile=outDir + "/" + parser.acc1c + "_specific_k" + str(parser.kmerSize) + ".fa")
 
-write_fasta(kmer_dict=acc2in_dict,
+write_fasta(kmer_dict=acc2c_kmers_dict,
             acc_name=parser.acc2c[0:5],
             outfile=outDir + "/" + parser.acc2c + "_specific_k" + str(parser.kmerSize) + ".fa")
 
-write_fasta(kmer_dict=acc1out_dict,
+write_fasta(kmer_dict=acc1nc_kmers_dict,
             acc_name=parser.acc1nc[0:5],
             outfile=outDir + "/" + parser.acc1nc + "_specific_k" + str(parser.kmerSize) + ".fa")
 
-write_fasta(kmer_dict=acc2out_dict,
+write_fasta(kmer_dict=acc2nc_kmers_dict,
             acc_name=parser.acc2nc[0:5],
             outfile=outDir + "/" + parser.acc2nc + "_specific_k" + str(parser.kmerSize) + ".fa")
-
-
-# Align accession-specific read segments to respective genome
-def align_read_segment_mm_sr(segment_fasta, genome):
-    """
-    Align read in FASTA format to genome using minimap2 sr.
-    """
-    aln_cmd = ["minimap2"] + \
-              ["-x", "sr"] + \
-              ["-t", "1"] + \
-              ["-p", "1.0"] + \
-              ["-N", "10"] + \
-              ["index/" + genome + ".fa"] + \
-              [segment_fasta]
-    outpaf = re.sub(".fasta", "_mm_sr.paf", segment_fasta)
-    outerr = re.sub(".fasta", "_mm_sr.err", segment_fasta)
-    with open(outpaf, "w") as outfile_handle, open(outerr, "w") as outerr_handle:
-        subprocess.run(aln_cmd, stdout=outfile_handle, stderr=outerr_handle)
-    # Delete file(s) if unmapped
-    if os.stat(outpaf).st_size == 0:
-        subprocess.run(["rm", outpaf, outerr])
 
 
 # Align accession-specific k-mers to respesctive genome
@@ -320,11 +299,12 @@ def align_kmers_bowtie(kmers_fasta, genome):
     with open(out_sam, "w") as outfile_handle, open(out_err, "w") as outerr_handle:
         subprocess.run(aln_cmd, stdout=outfile_handle, stderr=outerr_handle)
 
-# Sort and convert SAM into BAM
+
+# Sort and convert SAM into BAM, and delete SAM
 def sam_to_bam(in_sam):
     #in_sam = outDir + "/" + parser.acc1nc + "_specific_k" + str(parser.kmerSize) + "_bowtie.sam"
     """
-    Sort and convert SAM into BAM.
+    Sort and convert SAM into BAM, and delete SAM
     """
     out_bam = re.sub(".sam", ".bam", in_sam)
     out_err = re.sub(".sam", "_sam2bam.err", in_sam)
@@ -335,4 +315,27 @@ def sam_to_bam(in_sam):
               [in_sam]
     with open(out_err, "w") as outerr_handle:
         subprocess.run(bam_cmd, stderr=outerr_handle)
+        subprocess.run(["rm", in_sam])
+
+
+# Align accession-specific k-mers to respective genome
+# Sort and convert SAM into BAM, and delete SAM
+# acc1c_kmers
+align_kmers_bowtie(kmers_fasta=outDir + "/" + parser.acc1c + "_specific_k" + str(parser.kmerSize) + ".fa",
+                   genome=re.sub(r"(_scaffolds)_.+", r"\1", parser.acc1c))
+sam_to_bam(outDir + "/" + parser.acc1c + "_specific_k" + str(parser.kmerSize) + "_bowtie.sam")
+# acc2c_kmers
+align_kmers_bowtie(kmers_fasta=outDir + "/" + parser.acc2c + "_specific_k" + str(parser.kmerSize) + ".fa",
+                   genome=re.sub(r"(_scaffolds)_.+", r"\1", parser.acc2c))
+sam_to_bam(outDir + "/" + parser.acc2c + "_specific_k" + str(parser.kmerSize) + "_bowtie.sam")
+
+# acc1nc_kmers
+align_kmers_bowtie(kmers_fasta=outDir + "/" + parser.acc1nc + "_specific_k" + str(parser.kmerSize) + ".fa",
+                   genome=re.sub(r"(_scaffolds)_.+", r"\1", parser.acc1nc))
+sam_to_bam(outDir + "/" + parser.acc1nc + "_specific_k" + str(parser.kmerSize) + "_bowtie.sam")
+# acc2nc_kmers
+align_kmers_bowtie(kmers_fasta=outDir + "/" + parser.acc2nc + "_specific_k" + str(parser.kmerSize) + ".fa",
+                   genome=re.sub(r"(_scaffolds)_.+", r"\1", parser.acc2nc))
+sam_to_bam(outDir + "/" + parser.acc2nc + "_specific_k" + str(parser.kmerSize) + "_bowtie.sam")
+
 
