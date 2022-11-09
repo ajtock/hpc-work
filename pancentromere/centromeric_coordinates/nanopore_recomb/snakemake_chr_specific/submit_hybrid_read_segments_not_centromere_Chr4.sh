d94fac05067ac9d909959a556f65493dab424fbb
ajtock <andytock@gmail.com>
2022-11-09 12:10:54 +0000

    snakemake_chr_specific

--- pancentromere/centromeric_coordinates/nanopore_recomb/snakemake_chr_specific/submit_hybrid_read_segments_not_centromere_Chr4.sh
+++ pancentromere/centromeric_coordinates/nanopore_recomb/snakemake_chr_specific/submit_hybrid_read_segments_not_centromere_Chr4.sh
@@ -6,5 +6,5 @@ conda activate python_3.9.6
 echo $(which snakemake)
 echo $(which python)
 echo $(which minimap2)
-snakemake --configfile config_not_centromere_Chr4.yaml --printshellcmds --profile profile/
+snakemake --configfile config_not_centromere_Chr4.yaml --profile profile/
 conda deactivate
