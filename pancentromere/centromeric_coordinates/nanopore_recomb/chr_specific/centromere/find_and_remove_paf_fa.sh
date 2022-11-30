4ca9b7bf0d888e9036d3e165ab7d5b2cf649ea73
ajtock <andytock@gmail.com>
2022-11-30 08:28:40 +0000

    chr_specific

---
+++ pancentromere/centromeric_coordinates/nanopore_recomb/chr_specific/centromere/find_and_remove_paf_fa.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for CHROM in $(seq 1 5);
+do
+  echo Chr${CHROM};
+  echo Removing Col-0 segment PAFs and FASTAs;
+  find Chr${CHROM}/segments/Col-0/co/ -mindepth 1 -maxdepth 1 -type f -name "*.*a*" -exec rm {} + ;
+  find Chr${CHROM}/segments/Col-0/nco/ -mindepth 1 -maxdepth 1 -type f -name "*.*a*" -exec rm {} + ;
+  echo Removing Ler-0 segment PAFs and FASTAs;
+  find Chr${CHROM}/segments/Ler-0/co/ -mindepth 1 -maxdepth 1 -type f -name "*.*a*" -exec rm {} + ;
+  find Chr${CHROM}/segments/Ler-0/nco/ -mindepth 1 -maxdepth 1 -type f -name "*.*a*" -exec rm {} + ;
+done 
